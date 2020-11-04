3f2f9eafa895c2dda551c60565d1458d6b05a81d
KY0825 <mouko.ky@gmail.com>
2020-11-04 16:44:51 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'groups#index'
   devise_for :users
-  resources :groups, only: [:new, :index, :create] 
-  # do
-  #   resources :messages, only: [:index, :edit]
-  # end
+  resources :groups, only: [:new, :index, :create, :show] do
+    resources :messages, only: [:index, :edit, :show]
+  end
 end
