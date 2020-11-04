31047f0767d0cc458777e6f784ca03dee02f67cd
KY0825 <mouko.ky@gmail.com>
2020-11-04 10:48:21 +0900

    groups_controllerの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,27 @@
 class GroupsController < ApplicationController
-  def index
+  
+  def new
+    @group = Group.new
   end
+  
+  def index
+    @groups = Group.all
+    @groups = Group.order("created_at DESC")
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name, :text, :category_id, :genre_id, :image)
+    end
+
 end
