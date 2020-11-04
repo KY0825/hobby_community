2dfb7bdef121a6f4878a2a4877ba426d0895a96e
KY0825 <mouko.ky@gmail.com>
2020-11-04 10:48:55 +0900

    userの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+
+  validates :nickname,         presence: true
+  validates :password,         presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+  validates :last_name
+  validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  validates :birthday,         presence: true
+
   has_many :group_users
   has_many :groups, through: :group_users
-       
+  has_many :messages
 end
